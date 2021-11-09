fb778557597df3c37e82213f903f3257c0aee060
LAPTOP-J1R7PAL8\inadir <nirakli@gmail.com>
2021-11-09 14:15:23 +0400

    ff

---
+++ Application/Domains/Client/Client/Queries/GetClientList/ClientView.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Domains.Client.Client.Queries.GetClientList
+{
+    class ClientView
+    {
+
+    }
+}
---
+++ Application/Domains/Client/Client/Queries/GetClientList/GetClientList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+using DataAccessLayer.Model.Client;
+using DataAccessLayer;
+
+using Application.Common.Interfaces;
+using Application.Common.Exceptions;
+
+namespace Application.Domains.Client.Client.Queries.GetClientList
+{
+
+    public class GetClientListQuery : IRequest<int>
+    {
+        int? id;
+        int? topRecords;
+        string firstName;
+        string lastName;
+        string pid;
+        DateTime? bodFrom;
+        DateTime? bodTo;
+        int? clientTypeID;
+    }
+
+    class GetClientListQueryHandler: IRequestHandler<GetClientListQuery, ClientView>
+    {
+
+        private readonly IMediator _mediator;
+        private readonly ICoreDBContext _context;
+
+        public GetClientListQueryHandler(IMediator mediator, ICoreDBContext context)
+        {
+            _mediator = mediator;
+            _context = context;
+        }
+
+        public async Task<ClientView> Handle(GetClientListQuery request, CancellationToken cancellationToken)
+        {
+//            var customers = await _context.Customers
+//                .ProjectTo<CustomerLookupDto>(_mapper.ConfigurationProvider)
+//                .ToListAsync(cancellationToken);
+
+//            var vm = new CustomersListVm
+//            {
+//                Customers = customers
+//            };
+
+            return 0;
+        }
+
+    }
+}
